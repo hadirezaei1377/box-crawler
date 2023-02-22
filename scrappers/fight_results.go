be26e3cfd30649d512e8e7bde4a728b2096f07b7
Arsham Arya <Arshamalh@gmail.com>
2023-02-22 23:30:40 +0330

    Fix: fight results namings.

--- scrappers/fight_results.go
+++ scrappers/fight_results.go
@@ -1,24 +1,24 @@
 package scrappers
 
 import (
-	"box-crawler/database"
-	"box-crawler/entities"
-	"net/http"
+	"fmt"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 const (
-	_FightDate      = ""
-	_FightDateAlter = ""
-	_FighterName1   = "header:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(2) > span:nth-child(1)"
-	_FighterName2   = "header:nth-child(1) > div:nth-child(3) > div:nth-child(3) > div:nth-child(1) > span:nth-child(2)"
-	_FighterScore1  = "header:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(2)"
-	_FighterScore2  = "header:nth-child(1) > div:nth-child(3) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1)"
-	_FightInfo      = "div.schedule-card:nth-child(7) > header:nth-child(1) > div:nth-child(5) > a:nth-child(1)"
-	_MatchLocation  = "div:nth-child(2) > p:nth-child(1)"
-	_ScoreCards     = "div:nth-child(2) > p:nth-child(2)"
+	_FR_FightDate      = ""
+	_FR_FightDateAlter = ""
+	_FR_FighterName1   = "header:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(2) > span:nth-child(1)"
+	_FR_FighterName2   = "header:nth-child(1) > div:nth-child(3) > div:nth-child(3) > div:nth-child(1) > span:nth-child(2)"
+	_FighterScore1     = "header:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(2)"
+	_FighterScore2     = "header:nth-child(1) > div:nth-child(3) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1)"
+	_FR_FightInfo      = "div.schedule-card:nth-child(7) > header:nth-child(1) > div:nth-child(5) > a:nth-child(1)"
+	_FR_MatchLocation  = "div:nth-child(2) > p:nth-child(1)"
+	_ScoreCards        = "div:nth-child(2) > p:nth-child(2)"
 )
 
-func (sc scrapper) ScrapeUpcomingFights() {
+func (sc scrapper) ScrapeFightResults() {
 	doc, err := get("https://box.live/fight-results")
 	if err != nil {
 		fmt.Println("cannot get fight results: ", err)
@@ -36,9 +36,9 @@ func (sc scrapper) ScrapeUpcomingFights() {
 			"fighter_name_2": h.Find(_FighterName2).Text(),
 			"fight_info":     h.Find(_FightInfo).Text(),
 			"FighterScore1":  h.Find(_FighterScore1).Text(),
-			"FighterScore2":  h.Find(__FighterScore2).Text(),
+			"FighterScore2":  h.Find(_FighterScore2).Text(),
 			"match_location": h.Find(_MatchLocation).Text(),
-			"ScoreCards":     h.Find(__ScoreCards).Text(),
+			"ScoreCards":     h.Find(_ScoreCards).Text(),
 		})
 	})
 
