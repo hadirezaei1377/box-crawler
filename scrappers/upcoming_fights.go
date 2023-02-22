9165d2d71f9e51d49aba6956d1d4bbe8d00bdcdc
Arsham Arya <Arshamalh@gmail.com>
2023-02-22 23:04:33 +0330

    Add: upcoming fights scrapper.

--- scrappers/upcoming_fights.go
+++ scrappers/upcoming_fights.go
@@ -2,30 +2,39 @@ package scrappers
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/PuerkitoBio/goquery"
+)
+
+const (
+	_FightDate      = "h3.site-content__section__subtitle:nth-child(1)"
+	_FightDateAlter = "h3.site-content__section__subtitle:nth-child(6)"
+	_FighterName1   = "header:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(2) > span:nth-child(1)"
+	_FighterName2   = "header:nth-child(1) > div:nth-child(3) > div:nth-child(3) > div:nth-child(1) > span:nth-child(2)"
+	_FightInfo      = "div.schedule-card:nth-child(7) > header:nth-child(1) > div:nth-child(5) > a:nth-child(1)"
+	_MatchLocation  = "div:nth-child(2) > p:nth-child(1)"
 )
 
 func (sc scrapper) ScrapeUpcomingFights() {
 	doc, err := get("https://box.live/upcoming-fights-schedule")
 	if err != nil {
-		log.Fatal(">> ", err)
+		fmt.Println("cannot get upcoming fights: ", err)
 	}
 
-	fmt.Println(doc.Find(_Date).Text())
-
 	selection := doc.Find("#this-week > div").Each(func(i int, h *goquery.Selection) {
-		fight_date := h.Find("h3.site-content__section__subtitle:nth-child(1)").Text()
+		fight_date := h.Find(_FightDate).Text()
 		if fight_date == "" {
-			fight_date = h.Find("h3.site-content__section__subtitle:nth-child(6)").Text()
+			fight_date = h.Find(_FightDateAlter).Text()
 		}
-		fighter_name1 := h.Find("header:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(2) > span:nth-child(1)").Text()
-		fighterName2 := h.Find("header:nth-child(1) > div:nth-child(3) > div:nth-child(3) > div:nth-child(1) > span:nth-child(2)").Text()
-		fightInfo := h.Find("div.schedule-card:nth-child(7) > header:nth-child(1) > div:nth-child(5) > a:nth-child(1)").Text()
-		matchLocation := h.Find("div:nth-child(2) > p:nth-child(1)").Text()
-		fmt.Println(fighter_name1, fighterName2, fightInfo, matchLocation)
 
+		sc.db.InsertUpcomingFight(map[string]string{
+			"fight_date":     fight_date,
+			"fighter_name_1": h.Find(_FighterName1).Text(),
+			"fighter_name_2": h.Find(_FighterName2).Text(),
+			"fight_info":     h.Find(_FightInfo).Text(),
+			"match_location": h.Find(_MatchLocation).Text(),
+		})
 	})
+
 	fmt.Println(">>", selection)
 }
