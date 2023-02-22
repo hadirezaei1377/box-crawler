76024bd76ee240fe06ee703a0d5974663bd7a403
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2023-02-22 23:20:53 +0330

    my changes

--- scrappers/fight_results.go
+++ scrappers/fight_results.go
@@ -1,21 +1,46 @@
 package scrappers
 
-func (sc scrapper) ScrapeFightResults() {
-	get("https://box.live/fight-results")
+import (
+	"box-crawler/database"
+	"box-crawler/entities"
+	"net/http"
+)
 
-	// d.OnHTML("#latest-results", func(container *colly.HTMLElement) {
-	// 	container.ForEach("div", func(i int, h *colly.HTMLElement) {
-	// 		fightDate := h.ChildText("?")
-	// 		fighterName1 := h.ChildText("header:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(2) > span:nth-child(1)")
-	// 		fighterName2 := h.ChildText("header:nth-child(1) > div:nth-child(3) > div:nth-child(3) > div:nth-child(1) > span:nth-child(2)")
-	// 		fighterScore1 := h.ChildText("header:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(2)")
-	// 		fighterScore2 := h.ChildText("header:nth-child(1) > div:nth-child(3) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1)")
-	// 		fightInfo := h.ChildText("div.schedule-card:nth-child(7) > header:nth-child(1) > div:nth-child(5) > a:nth-child(1)")
-	// 		matchLocation := h.ChildText("div:nth-child(2) > p:nth-child(1)")
-	// 		scoreCards := h.ChildText("div:nth-child(2) > p:nth-child(2)")
-	// 		fmt.Println(fightDate, fighterName1, fighterName2, fighterScore1, fighterScore2, fightInfo, matchLocation, scoreCards)
-	// 	})
-	// })
+const (
+	_FightDate      = ""
+	_FightDateAlter = ""
+	_FighterName1   = "header:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(2) > span:nth-child(1)"
+	_FighterName2   = "header:nth-child(1) > div:nth-child(3) > div:nth-child(3) > div:nth-child(1) > span:nth-child(2)"
+	_FighterScore1  = "header:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(2)"
+	_FighterScore2  = "header:nth-child(1) > div:nth-child(3) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1)"
+	_FightInfo      = "div.schedule-card:nth-child(7) > header:nth-child(1) > div:nth-child(5) > a:nth-child(1)"
+	_MatchLocation  = "div:nth-child(2) > p:nth-child(1)"
+	_ScoreCards     = "div:nth-child(2) > p:nth-child(2)"
+)
 
-	// sc.db.UpsertFightResults(map[string]string{})
+func (sc scrapper) ScrapeUpcomingFights() {
+	doc, err := get("https://box.live/fight-results")
+	if err != nil {
+		fmt.Println("cannot get fight results: ", err)
+	}
+
+	selection := doc.Find("div.schedule-card > div").Each(func(i int, h *goquery.Selection) {
+		fight_date := h.Find(_FightDate).Text()
+		if fight_date == "" {
+			fight_date = h.Find(_FightDateAlter).Text()
+		}
+
+		sc.db.InsertUpcomingFight(map[string]string{
+			"fight_date":     fight_date,
+			"fighter_name_1": h.Find(_FighterName1).Text(),
+			"fighter_name_2": h.Find(_FighterName2).Text(),
+			"fight_info":     h.Find(_FightInfo).Text(),
+			"FighterScore1":  h.Find(_FighterScore1).Text(),
+			"FighterScore2":  h.Find(__FighterScore2).Text(),
+			"match_location": h.Find(_MatchLocation).Text(),
+			"ScoreCards":     h.Find(__ScoreCards).Text(),
+		})
+	})
+
+	fmt.Println(">>", selection)
 }
